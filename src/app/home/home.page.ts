acfc413484b3115a873fbeb3252fca4e4a9db147
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2021-10-28 14:19:20 +0200

    Pruebas con el scanner.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,83 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() {}
+  result = null;
+  scanActive = false;
+
+  constructor(
+    private alertCtrl: AlertController,
+  ) {}
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    BarcodeScanner.prepare();
+  }
+
+  async startScanner() {
+    const allowed = await this.checkPermission();
+
+    if (allowed) {
+      this.scanActive = true;
+      const result = await BarcodeScanner.startScan();
+
+      if (result.hasContent) {
+        this.result = result.content;
+        this.scanActive = false;
+      }
+    }
+  }
+
+  async checkPermission() {
+    return new Promise(async (resolve, reject) => {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+
+      if (status.granted) {
+        // We are fine
+        resolve(true);
+      } else if (status.denied) {
+        // Denied before
+        const alert = await this.alertCtrl.create({
+          header: 'No permission',
+          message: 'Please allow camera access in your settings',
+          buttons: [{
+            text: 'No',
+            role: 'cancel'
+          },
+          {
+            text: 'Open Settings',
+            handler: () => {
+              resolve(false);
+              BarcodeScanner.openAppSettings();
+            }
+          }]
+        });
+
+        await alert.present();
+      } else {
+        resolve(false);
+      }
+    });
+  };
+
+  stopScanner() {
+    BarcodeScanner.stopScan();
+    this.scanActive = false;
+  }
+
+  ngOnDestroy() {
+    BarcodeScanner.stopScan();
+  }
 
 }
