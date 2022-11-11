937cc6621fca9d7c5933937bc074696ab6d6b3fe
Gino Luiggi Chirinos Plasencia <lchirinos@acercapartners.com>
2022-11-11 06:17:10 +0100

    cambios

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { AlertController, Platform } from '@ionic/angular';
+import { AlertController, LoadingController, Platform } from '@ionic/angular';
 
-import { DataService } from '../../services/data.service';
-
+import ApkUpdater, {Update} from 'cordova-plugin-apkupdater';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -13,14 +13,176 @@ import { DataService } from '../../services/data.service';
 export class HomePage implements OnInit {
   compounds: any[] = [];
 
+  remoteUrl: string;
+  remoteVersion: any;
+  installedVersion: any;
+  remoteVersionCode: any;
+  installedVersionCode: any;
+  showUpdateInfo = false;
+  hasError = false;
+  jsonError: string;
+
   constructor(
-    private dataService: DataService,
-  ) { }
+    private platform: Platform,
+    private alertCtrl: AlertController,
+    private http: HttpClient,
+    private loadingCtrl: LoadingController
+  ) {
+    // this.platform.ready().then(() => {
+    //   console.log('PLATFORM READY');
+
+    //   this.update();
+    // });
+  }
 
   async ngOnInit() {
-    this.dataService.getCompounds()
-      .subscribe(res => {
-        this.compounds = res;
-      });
+  }
+
+  async update() {
+    // const url = this.remoteUrl + '/update.json';
+    // const url = 'assets/data/app-update/update.json';
+    const url = 'https://api.wepark.pro/api/app-update-info';
+    const manifest = await this.http.get<Update>(url).toPromise();
+
+
+
+    this.showUpdateInfo = true;
+    this.remoteVersion = manifest.app.version.name;
+    this.installedVersion = (await ApkUpdater.getInstalledVersion()).version.name;
+
+    this.remoteVersionCode = manifest.app.version.code;
+    this.installedVersionCode = (await ApkUpdater.getInstalledVersion()).version.code;
+
+    // if (remoteVersionCode > installedVersionCode) {
+        // await ApkUpdater.download(
+        //     this.remote + '/update.zip',
+        //     {
+        //         zipPassword: 'aDzEsCceP3BPO5jy',
+        //         onDownloadProgress: console.log,
+        //         onUnzipProgress: console.log
+        //     }
+        // );
+
+        // ApkUpdater.download(
+        //   'https://raw.githubusercontent.com/kolbasa/cordova-plugin-apkupdater-demo/master/update/update.zip',
+        //     {
+        //         zipPassword: 'aDzEsCceP3BPO5jy',
+                // onDownloadProgress: (e) => {
+                //     console.log('Downloading: ' + e.progress + '%');
+                // },
+        //         onUnzipProgress: (e) => {
+        //             console.log('Unzipping: ' + e.progress + '%');
+        //         }
+        //     },
+        //     (resp) => {
+        //         console.log('Update can be installed now', resp);
+        //     },
+        // );
+
+      // if (remoteVersionCode > installedVersionCode) {
+      if (true) {
+        const loading = await this.loadingCtrl.create({ message: `Descargando... <span>0</span>%` });
+
+        await loading.present();
+
+        ApkUpdater.download(
+          'https://api.wepark.pro/api/app-update-download',
+          {
+            // zipPassword: 'secret',
+            onDownloadProgress: (e) => {
+              loading.querySelector('.loading-content span').innerHTML = `${e.progress}`;
+              console.log('Downloading: ' + e.progress + '%');
+            },
+            // onUnzipProgress: (e) => {
+            //   console.log('Unzipping: ' + e.progress + '%');
+            // }
+          },
+          async () => {
+              this.jsonError = null;
+              this.hasError = false;
+              await loading.dismiss();
+
+              const alert = await this.alertCtrl.create({ message: 'Descarga completada. ¿Desea instalar la actualización?' });
+
+              await alert.present();
+
+              await alert.onDidDismiss();
+
+              ApkUpdater.install();
+          },
+          async (err) => {
+            this.jsonError = JSON.stringify(err);
+            this.hasError = true;
+            await loading.dismiss();
+
+            const alert = await this.alertCtrl.create({ message: 'Ocurrió un problema en la descarga' });
+
+            await alert.present();
+          }
+        );
+      }
+
+      // WITH ZIP
+
+      // ApkUpdater.download(
+      //   'https://raw.githubusercontent.com/kolbasa/cordova-plugin-apkupdater-demo/master/update/update.zip',
+      //   {
+      //     zipPassword: 'aDzEsCceP3BPO5jy',
+      //     onDownloadProgress: (e) => {
+      //       loading.querySelector('.loading-content span').innerHTML = `${e.progress}`;
+      //       console.log('Downloading: ' + e.progress + '%');
+      //     },
+      //     onUnzipProgress: (e) => {
+      //       console.log('Unzipping: ' + e.progress + '%');
+      //     }
+      //   },
+      //   async () => {
+      //       await loading.dismiss();
+
+      //       const alert = await this.alertCtrl.create({ message: 'Descarga completada. ¿Desea instalar la actualización?' });
+
+      //       await alert.present();
+
+      //       await alert.onDidDismiss();
+
+      //       ApkUpdater.install();
+      //   },
+      //   async () => {
+      //     await loading.dismiss();
+
+      //     const alert = await this.alertCtrl.create({ message: 'Ocurrió un problema en la descarga' });
+
+      //     await alert.present();
+      //   }
+      // );
+
+      // With APK
+      // ApkUpdater.download(
+      //   'https://github.com/kolbasa/cordova-plugin-apkupdater-demo/raw/master/Demo.apk',
+      //   {
+      //     onDownloadProgress: (e) => {
+      //       loading.querySelector('.loading-content span').innerHTML = `${e.progress}`;
+      //       console.log('Downloading: ' + e.progress + '%');
+      //     },
+      //   },
+      //   async () => {
+      //       await loading.dismiss();
+
+      //       const alert = await this.alertCtrl.create({ message: 'Descarga completada. ¿Desea instalar la actualización?' });
+
+      //       await alert.present();
+
+      //       await alert.onDidDismiss();
+
+      //       ApkUpdater.install();
+      //   },
+      //   async () => {
+      //     await loading.dismiss();
+
+      //     const alert = await this.alertCtrl.create({ message: 'Ocurrió un problema en la descarga' });
+
+      //     await alert.present();
+      //   }
+      // );
   }
 }
